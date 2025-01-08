30630d9abb2542dc376762e64461e6b289c534a7
shc-sv <83458693+shc-sv@users.noreply.github.com>
2025-01-08 16:29:42 +0100

    This my first commit.

---
+++ main.py
@@ -0,0 +1,19 @@
+# This is a sample Python script.
+
+# Press Skift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    #print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'This Python program was made by {name}.' )
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print("Hello world!")
+    name = input("Vem är du? Skirv ditt namn här: ")
+    print_hi(name)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
