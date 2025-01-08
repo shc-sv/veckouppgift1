d9cc49d54f68a6e0945703c78dacc5847d13bbb5
shc-sv <83458693+shc-sv@users.noreply.github.com>
2025-01-08 16:57:06 +0100

    The first question is done.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# This is a sample Python script.
+# Vecouppgift 1
 
 # Press Skift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
@@ -6,14 +6,13 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    #print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     print(f'This Python program was made by {name}.' )
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print("Hello world!")
-    name = input("Vem är du? Skirv ditt namn här: ")
+    name = input("Who did this program? Please give me the name here: ")
     print_hi(name)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
